8b46ce8b725b299a7c65353c90c39749ed068593
ArunRAIN <arununique7@gmail.com>
2018-06-25 16:26:08 +0530

    new

--- src/test/java/com/bdd/sample/Genric.java
+++ src/test/java/com/bdd/sample/Genric.java
@@ -3,13 +3,6 @@ package com.bdd.sample;
 import java.io.FileInputStream;
 import java.util.Properties;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.serenitybdd.core.pages.WebElementFacade;
-
 public class Genric {
 	public static String getPropertyValue(String key)
 	{
